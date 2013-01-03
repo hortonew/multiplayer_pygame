7b65cf25a2085dcf76a406936443511b97cbea89
Erik Horton <hortonew@gmail.com>
2013-01-02 22:21:59 -0500

    comments

--- server.py
+++ server.py
@@ -1,20 +1,17 @@
-import pygame
-from pygame.locals import *
 from classes import Character, Characters
 from gevent import socket
 from gevent.server import StreamServer
 
 class Server():
 	def __init__(self):
-		self.running = False
-		self.clock = pygame.time.Clock()
-		pygame.init()
 		
+		#get server ip (current machine)
 		self.ip = self.ip = socket.gethostbyname(socket.gethostname())
 		print "Starting server: %s:1337" % self.ip
 		
 		self.chs = Characters.Characters()
 		
+	#take data from client and return new positions
 	def update():
 		pass
 	
@@ -23,6 +20,7 @@ class Server():
 		newch = self.newCharacter(l+1, address[0])
 		self.chs.addCharacter(newch)
 
+		#create file object to talk to socket
 		fp = sock.makefile()
 		while True:
 			line = fp.read(1)
@@ -30,28 +28,21 @@ class Server():
 				print l, " - IP: ", address[0], ". Port: ", address[1], ". ", line
 			else:
 				break
+		#close connection and remove character from character list
 		sock.shutdown(socket.SHUT_WR)
 		sock.close()
 		self.chs.removeCharacter(newch)
-		print "Closing...", address[0]
+		print "Closing connection to: ", address[0]
 		
+	#return character object
 	def newCharacter(self, id, ip):
 		ch = Character.Character(id, ip)
 		return ch
-		
-	def handleEvents(self):
-		pass
 
-	def start(self, fps=0):
-		self.running = True
-		self.fps = fps
+	#start server
+	def start(self):
 		server = StreamServer( ('', 1337), self.clientConnect)
 		server.serve_forever()
-		
-		while self.running:
-			self.handleEvents()
-			self.clock.tick(self.fps)
 			
-fps = 30
 s = Server()
-s.start(fps)
+s.start()
